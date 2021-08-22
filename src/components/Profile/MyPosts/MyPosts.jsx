c05221feedbdc6117ce537b4868a534d888c6311
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-22 22:45:29 +0300

    15. Виділив пости в окрему компоненту.

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import s from './MyPosts.module.css'
+import Post from './Post/Post';
 
 const MyPosts = () => {
   return <div className={s.content}>
     <div>
       My posts
-        <div>
-        New Post
-        </div>
+      <div>
+        <textarea></textarea>
+        <button>Add post</button>
+      </div>
       <div className={s.posts}>
-        <div className={s.item}>post 1</div>
-        <div className={s.item}>post 2</div>
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   </div>
