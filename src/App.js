700d010d1521a8b48754084b7d7d6610976c1b6f
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-23 14:22:03 +0300

    BrouserRouter, перемикання між Profile та Messages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
@@ -7,14 +8,17 @@ import Profile from './components/Profile/Profile';
 
 const App = () => {
     return (
-        <div className="app-audi" >
-            <Header />
-            <Navbar />
-            <div className="app-audi-content">
-                <Dialogs />
-                {/* <Profile /> */}
+        <BrowserRouter>
+            <div className="app-audi" >
+                <Header />
+                <Navbar />
+                <div className="app-audi-content">
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/dialogs' component={Dialogs} />
+
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
