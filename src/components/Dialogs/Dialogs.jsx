aa4ae70364f1cab51140e05cc5b497caca7eed4a
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-24 18:29:23 +0300

    21. Зверстав сторінку Діалогів

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,38 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                    Танька
+                </div>
+                <div className={s.dialog}>
+                    Сергій
+                </div>
+                <div className={s.dialog}>
+                    Толік
+                </div>
+                <div className={s.dialog}>
+                    Саша
+                </div>
+                <div className={s.dialog}>
+                    Вася
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How is your ReactJS?
+                </div>
+                <div className={s.message}>
+                    Прогресує.
+                </div>
+            </div>
         </div>
     )
 }
