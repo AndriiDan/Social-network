1b723e3b90e95673171694e4e06296c59130f0c2
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-26 00:33:50 +0300

    24. Масиви для Dialogs та Messages.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,19 +19,39 @@ const Message = (props) => {
     )
 }
 
+
+
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        { id: 1, name: 'Танька' },
+        { id: 2, name: 'Сергій' },
+        { id: 3, name: 'Толік' },
+        { id: 4, name: 'Саша' },
+        { id: 5, name: 'Вася' },
+        { id: 6, name: 'Dimych' }
+    ]
+
+    let messagesData = [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How is your ReactJS?' },
+        { id: 3, message: 'Прогресує.' }
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <DialogItem name='Танька' id='1' />
-                <DialogItem name='Сергій' id='2' />
-                <DialogItem name='Толік' id='3' />
-                <DialogItem name='Саша' id='4' />
-                <DialogItem name='Вася' id='5' />
-                <DialogItem name='Dimych' id='6' />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
             </div>
             <div className={s.messages}>
-                <Message message='Hi' />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                <Message message={messagesData[2].message} />
                 <Message message='How is your ReactJS?' />
                 <Message message='Прогресує.' />
             </div>
