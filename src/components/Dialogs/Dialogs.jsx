e9631eba80999109c129c93ffefd7605242d5bf0
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-26 18:41:39 +0300

    25. Масив компонент в Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -32,28 +32,34 @@ const Dialogs = (props) => {
         { id: 6, name: 'Dimych' }
     ]
 
+    let dialogsElements = [
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />,
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />,
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />,
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />,
+        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />,
+        <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+    ]
+
     let messagesData = [
         { id: 1, message: 'Hi' },
         { id: 2, message: 'How is your ReactJS?' },
         { id: 3, message: 'Прогресує.' }
     ]
 
+    let MessagesElements = [
+        <Message message={messagesData[0].message} />,
+        <Message message={messagesData[1].message} />,
+        <Message message={messagesData[2].message} />
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} />
-                <Message message='How is your ReactJS?' />
-                <Message message='Прогресує.' />
+                {MessagesElements}
             </div>
         </div>
     )
