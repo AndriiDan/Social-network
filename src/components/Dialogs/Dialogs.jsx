db7bc1248901ab1ea2bc0e361cb7560eca75ae5d
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-26 18:53:11 +0300

    25. Array.map() для Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -32,14 +32,8 @@ const Dialogs = (props) => {
         { id: 6, name: 'Dimych' }
     ]
 
-    let dialogsElements = [
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />,
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />,
-        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />,
-        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />,
-        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />,
-        <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
-    ]
+    let dialogsElements = dialogsData
+        .map(dialog => <DialogItem name={dialog.name} id={dialog.id} />)
 
     let messagesData = [
         { id: 1, message: 'Hi' },
@@ -47,11 +41,8 @@ const Dialogs = (props) => {
         { id: 3, message: 'Прогресує.' }
     ]
 
-    let MessagesElements = [
-        <Message message={messagesData[0].message} />,
-        <Message message={messagesData[1].message} />,
-        <Message message={messagesData[2].message} />
-    ]
+    let MessagesElements = messagesData
+        .map(message => <Message message={message.message} />)
 
     return (
         <div className={s.dialogs}>
