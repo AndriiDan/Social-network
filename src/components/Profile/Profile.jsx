2d1fb0284ac12ba5992d3567c07ea56daacfe3b2
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-29 21:22:03 +0300

    28. Виніс дані для MyPosts на рівень вверх

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,16 @@ import s from './Profile.module.css'
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
+
+  let posts = [
+    { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+    { id: 2, message: 'It\'s my first post.', likesCount: 20 }
+  ]
+
   return (
     <div>
       <ProfileInfo />
-      <MyPosts />
+      <MyPosts posts={posts} />
     </div>
   );
 }
