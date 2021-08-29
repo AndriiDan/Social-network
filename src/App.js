01289b13a13c4a6c27460ba57d3168a8ae9b9b7e
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-29 21:59:47 +0300

    28. Виніс дані posts, dialogs, messages в index.js

--- src/App.js
+++ src/App.js
@@ -9,36 +9,15 @@ import News from './components/News/News';
 import Profile from './components/Profile/Profile';
 import Settings from './components/Settings/Settings';
 
-const App = () => {
-
-    let posts = [
-        { id: 1, message: 'Hi, how are you?', likesCount: 15 },
-        { id: 2, message: 'It\'s my first post.', likesCount: 20 }
-    ]
-
-    let dialogs = [
-        { id: 1, name: 'Танька' },
-        { id: 2, name: 'Сергій' },
-        { id: 3, name: 'Толік' },
-        { id: 4, name: 'Саша' },
-        { id: 5, name: 'Вася' },
-        { id: 6, name: 'Dimych' }
-    ]
-
-    let messages = [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'How is your ReactJS?' },
-        { id: 3, message: 'Прогресує.' }
-    ]
-
+const App = (props) => {
     return (
         <BrowserRouter>
             <div className="app-audi" >
                 <Header />
                 <Navbar />
                 <div className="app-audi-content">
-                    <Route path='/profile' render={() => <Profile posts={posts} />} />
-                    <Route path='/dialogs' render={() => <Dialogs dialogs={dialogs} messages={messages} />} />
+                    <Route path='/profile' render={() => <Profile posts={props.posts} />} />
+                    <Route path='/dialogs' render={() => <Dialogs dialogs={props.dialogs} messages={props.messages} />} />
 
                     {/* <Route path='/profile' component={Profile} />
                     <Route path='/dialogs' component={Dialogs} /> */}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let posts = [
+  { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+  { id: 2, message: 'It\'s my first post.', likesCount: 20 }
+]
+
+let dialogs = [
+  { id: 1, name: 'Танька' },
+  { id: 2, name: 'Сергій' },
+  { id: 3, name: 'Толік' },
+  { id: 4, name: 'Саша' },
+  { id: 5, name: 'Вася' },
+  { id: 6, name: 'Dimych' }
+]
+
+let messages = [
+  { id: 1, message: 'Hi' },
+  { id: 2, message: 'How is your ReactJS?' },
+  { id: 3, message: 'Прогресує.' }
+]
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} dialogs={dialogs} messages={messages} />
   </React.StrictMode>,
   document.getElementById('root')
 );
