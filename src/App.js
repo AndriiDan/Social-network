e8db0028b908c792d25b7cdcc13f1e30e10bb6ec
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-08-29 20:31:33 +0300

    27. Метод render, щоб провести props через BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -16,8 +16,11 @@ const App = () => {
                 <Header />
                 <Navbar />
                 <div className="app-audi-content">
-                    <Route path='/profile' component={Profile} />
-                    <Route path='/dialogs' component={Dialogs} />
+                    <Route path='/profile' render={() => <Profile />} />
+                    <Route path='/dialogs' render={() => <Dialogs />} />
+
+                    {/* <Route path='/profile' component={Profile} />
+                    <Route path='/dialogs' component={Dialogs} /> */}
                     <Route path='/news' component={News} />
                     <Route path='/music' component={Music} />
                     <Route path='/settings' component={Settings} />
