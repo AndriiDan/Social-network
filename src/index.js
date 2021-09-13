6c1805ff146e1419785df7ad042f1c642b4f315f
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-09-13 12:55:28 +0300

    33. Створив файл render.js і виніс в нього функцію rerenderEntierTree.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {rerenderEntireTree} from "./render";
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
-import { addPost } from './redux/state';
-import { BrowserRouter } from 'react-router-dom';
 
-let rerenderEntireTree = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App state={state} addPost={addPost} />
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-rerenderEntireTree();
+rerenderEntireTree(state);
 
 
 
