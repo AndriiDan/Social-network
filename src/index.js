dc4f9358103359dabb164f15ebbb52c0cbadab7d
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-09-15 22:59:03 +0300

    35. Видалив render.js, функції переніс в index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import {rerenderEntireTree} from "./render";
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { addPost, updateNewPostText, addMessage, updateNewMessageText } from './redux/state';
+import { BrowserRouter } from 'react-router-dom';
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state}
+                    addPost={addPost} updateNewPostText={updateNewPostText}
+                    addMessage={addMessage} updateNewMessageText={updateNewMessageText} />
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+
 
 rerenderEntireTree(state);
 
