c57ef4e2cb31979efe7918da94db5e1a5a68ebe9
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-09-15 10:27:33 +0300

    34. Занулення поля вводу через ф-цію addPost в state.js

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,7 +9,6 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef();
   let addPost = () => {
     props.addPost();
-    props.updateNewPostText('');
   }
 
   let onPostChange = () => {
