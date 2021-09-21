602c91de2e1d5745ff0817e48aabc4947ac0a316
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-09-21 14:58:22 +0300

    39. Виніс константи наверх

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
         profilePage: {
@@ -43,7 +46,7 @@ let store = {
     },
 
     dispatch(action) {
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 3,
                 message: this._state.profilePage.newPostText,
@@ -53,7 +56,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         } else if (action.type === 'ADD-MESSAGE') {
@@ -75,7 +78,7 @@ let store = {
 
 export const addPostActionCreator = () => {
     return {
-        type: 'ADD-POST'
+        type: ADD_POST
     }
 }
 
