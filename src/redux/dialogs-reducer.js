1d04be7cfc15a34cafe1a6f58cd28278fa35d7c8
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-09-27 11:04:04 +0300

    42. store, redux-store

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,24 @@
 const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        { id: 1, name: 'Танька' },
+        { id: 2, name: 'Сергій' },
+        { id: 3, name: 'Толік' },
+        { id: 4, name: 'Саша' },
+        { id: 5, name: 'Вася' },
+        { id: 6, name: 'Dimych' }
+    ],
+    messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'How is your ReactJS?' },
+        { id: 3, message: 'Прогресує.' }
+    ],
+    newMessageText: 'it-kamasutra.com'
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_MESSAGE:
             let newMessage = {
