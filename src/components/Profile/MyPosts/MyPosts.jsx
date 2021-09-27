3e803fed45c96f3fc4ef9f54f1fd46a8824632cc
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-09-27 23:27:50 +0300

    43. MyPostsContainer

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
-import { addPostActionCreator, updateNewPostTextActionCreator } from './../../../redux/profile-reducer';
 
 const MyPosts = (props) => {
 
   let postsElement = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
 
   let newPostElement = React.createRef();
-  let addPost = () => {
-    props.dispatch(addPostActionCreator());
+
+  let onAddPost = () => {
+    props.addPost();
   }
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    let action = updateNewPostTextActionCreator(text);
-    props.dispatch(action);
+    props.updateNewPostText(text);
   }
 
   return <div className={s.content}>
@@ -28,7 +27,7 @@ const MyPosts = (props) => {
             onChange={onPostChange} />
         </div>
         <div>
-          <button onClick={addPost}>Add post</button>
+          <button onClick={onAddPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>
---
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
+import MyPosts from './MyPosts';
+
+const MyPostsContainer = (props) => {
+  let addPost = () => {
+    props.dispatch(addPostActionCreator());
+  }
+
+  let onPostChange = (text) => {
+    let action = updateNewPostTextActionCreator(text);
+    props.dispatch(action);
+  }
+
+  return (<MyPosts updateNewPostText={onPostChange} addPost={addPost} posts={props.posts} />)
+}
+
+export default MyPostsContainer;
