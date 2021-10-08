09d154850da27cb612163b0e38eaa1e0663cc92b
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-10-08 07:54:38 +0300

    45. connect для MyPostContainer, DialogsContainer

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,36 +1,56 @@
 import React from 'react';
 import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
-import StoreContext from '../../../StoreContext';
 import MyPosts from './MyPosts';
+import { connect } from "react-redux";
 
-const MyPostsContainer = () => {
-
-  return (
-    <StoreContext.Consumer>
-      { (store) => {
-        // let state = props.store.getState();
-        let state = store.getState();
-
-        // ф-ція, яка додає пост
-        let addPost = () => {
-          // props.store.dispatch(addPostActionCreator());
-          store.dispatch(addPostActionCreator());
-        }
-
-        // ф-ція, яка приймає текст з MyPosts і обновляє посимвольно в BLL
-        let onPostChange = (text) => {
-          let action = updateNewPostTextActionCreator(text);
-          // props.store.dispatch(action);
-          store.dispatch(action);
-        }
-        return <MyPosts
-          updateNewPostText={onPostChange}
-          addPost={addPost}
-          posts={state.profilePage.posts}
-          newPostText={state.profilePage.newPostText} />
-      }}
-    </StoreContext.Consumer>
-  )
+let mapStateToProps = (state) => {
+  return {
+    posts: state.profilePage.posts,
+    newPostText: state.profilePage.newPostText
+  }
 }
+
+let mapDispatchToProps = (dispatch) => {
+  return {
+    updateNewPostText: (text) => {
+      dispatch(updateNewPostTextActionCreator(text))
+    },
+    addPost: () => {
+      dispatch(addPostActionCreator())
+    }
+  }
+}
+
+const MyPostContainer = connect(mapStateToProps, mapDispatchTpProps)(MyPosts);
+
+// const MyPostsContainer = () => {
+
+//   return (
+//     <StoreContext.Consumer>
+//       { (store) => {
+//         // let state = props.store.getState();
+//         let state = store.getState();
+
+//         // ф-ція, яка додає пост
+//         let addPost = () => {
+//           // props.store.dispatch(addPostActionCreator());
+//           store.dispatch(addPostActionCreator());
+//         }
+
+//         // ф-ція, яка приймає текст з MyPosts і обновляє посимвольно в BLL
+//         let onPostChange = (text) => {
+//           let action = updateNewPostTextActionCreator(text);
+//           // props.store.dispatch(action);
+//           store.dispatch(action);
+//         }
+//         return <MyPosts
+//           updateNewPostText={onPostChange}
+//           addPost={addPost}
+//           posts={state.profilePage.posts}
+//           newPostText={state.profilePage.newPostText} />
+//       }}
+//     </StoreContext.Consumer>
+//   )
+// }
 
 export default MyPostsContainer;
