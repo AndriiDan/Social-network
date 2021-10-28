2b5b5895eca7d21b2bbea657719ff6b5c08e3133
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-10-28 23:14:10 +0300

    51. Бібліотека axios. get-запит для Users.jsx

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 
 const App = (props) => {
-    debugger;
     return (
         <div className="app-audi" >
             <Header />
