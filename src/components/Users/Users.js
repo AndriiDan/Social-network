a6d7b322f99574dced1ec97cdd4cea704827fff4
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-11-09 10:32:45 +0200

    56. Створив презинтаціну к-ту Users.js і переніс з UsersAPIComponent.jsx - 'jsx'

---
+++ src/components/Users/Users.js
@@ -0,0 +1,64 @@
+import React from "react";
+import userPhoto from "../../assets/image/user.png";
+import styles from "./Users.module.css";
+
+let Users = (props) => {
+
+    // інформація, про загальну к-сть сторінок
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    // створюємо масив сторінок
+    let pages = [];
+    // формуємо масив сторінок
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+
+    return <div>
+        <div>
+            {/* сформовуємо рядок з номерацією сторінок */}
+            {pages.map(p => {
+                return <span
+                    // виділяє активну сторінку
+                    className={props.currentPage === p && styles.selectedPage}
+                    // перемикає сторінку з юзерами
+                    onClick={() => { onPageChanged(p) }}>
+                    {p}
+                </span>
+            })}
+        </div>
+        {
+            props.users.map(u =>
+                <div key={u.id}>
+
+                    <span>
+                        <div>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
+                            //  width="50px"
+                            />
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => props.unfollow(u.id)} >Unfollow</button>
+                                : <button onClick={() => props.follow(u.id)} >Follow</button>}
+
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
+                        </span>
+                    </span>
+                </div>)
+        }
+
+    </div>
+
+}
+
+export default Users;
--- src/components/Users/UsersAPIComponent.jsx
+++ src/components/Users/UsersAPIComponent.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import * as axios from 'axios';
-import userPhoto from "../../assets/image/user.png";
-import styles from "./Users.module.css";
+import Users from './Users';
 
 
 class UsersAPIComponent extends React.Component {
@@ -24,61 +23,7 @@ class UsersAPIComponent extends React.Component {
     }
 
     render() {
-
-        // інформація, про загальну к-сть сторінок
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        // створюємо масив сторінок
-        let pages = [];
-        // формуємо масив сторінок
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return <div>
-            <div>
-                {/* сформовуємо рядок з номерацією сторінок */}
-                {pages.map(p => {
-                    return <span
-                        // виділяє активну сторінку
-                        className={this.props.currentPage === p && styles.selectedPage}
-                        // перемикає сторінку з юзерами
-                        onClick={() => { this.onPageChanged(p) }}>
-                        {p}
-                    </span>
-                })}
-            </div>
-            {
-                this.props.users.map(u =>
-                    <div key={u.id}>
-
-                        <span>
-                            <div>
-                                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
-                                //  width="50px"
-                                />
-                            </div>
-                            <div>
-                                {u.followed
-                                    ? <button onClick={() => this.props.unfollow(u.id)} >Unfollow</button>
-                                    : <button onClick={() => this.props.follow(u.id)} >Follow</button>}
-
-                            </div>
-                        </span>
-                        <span>
-                            <span>
-                                <div>{u.name}</div>
-                                <div>{u.status}</div>
-                            </span>
-                            <span>
-                                <div>{"u.location.country"}</div>
-                                <div>{"u.location.city"}</div>
-                            </span>
-                        </span>
-                    </div>)
-            }
-
-        </div>
+        return <Users />
     }
 }
 
