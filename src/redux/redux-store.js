a5e4e83c7f8cdd209cc85f0f54fe67698ff4d364
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-11-18 00:50:47 +0200

    59.

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,8 +1,8 @@
-import { combineReducers, createStore } from "redux";
-import dialogsReducer from "./dialogs-reducer";
-import profileReducer from "./profile-reducer";
-import sidebarReducer from "./sidebar-reducer";
-import usersReducer from "./users-reducer";
+import { combineReducers, createStore } from 'redux';
+import dialogsReducer from './dialogs-reducer';
+import profileReducer from './profile-reducer';
+import sidebarReducer from './sidebar-reducer';
+import usersReducer from './users-reducer';
 
 let redusers = combineReducers({
     profilePage: profileReducer,
