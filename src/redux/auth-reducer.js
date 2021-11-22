e849ff8b41ac6610aa6476db89a4c87936b47490
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-11-22 11:44:42 +0200

    61. Створив authReducer для login

---
+++ src/redux/auth-reducer.js
@@ -0,0 +1,24 @@
+const SET_USER_DATE = 'SET_USER_DATE';
+
+let initialState = {
+    id: null,
+    email: null,
+    login: null
+}
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER_DATE:
+            return {
+                ...state,
+                ...action.date
+            };
+        default:
+            return state;
+    }
+}
+
+export const setUserDate = (userId, email, login) =>
+    ({ type: SET_USER_DATE, date: { userId, email, login } })
+
+export default authReducer;
