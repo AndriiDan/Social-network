c821b2533c4a141d40c93c76c2f99770076e9d43
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-11-24 11:51:19 +0200

    62. Запити на сервер follow/unfollow

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import userPhoto from "../../assets/images/user.png";
@@ -40,8 +41,23 @@ let Users = (props) => {
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => props.unfollow(u.id)} >Unfollow</button>
-                                : <button onClick={() => props.follow(u.id)} >Follow</button>}
+                                ? <button onClick={() => {
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, { withCredentials: true }).
+                                        then(response => {
+                                            if (response.dada.resultCode == 0) {
+                                                props.unfollow(u.id)
+                                            }
+                                        });
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, { withCredentials: true }).
+                                        then(response => {
+                                            if (response.dada.resultCode == 0) {
+                                                props.follow(u.id)
+                                            }
+                                        });
+                                }}>Follow</button>
+                            }
 
                         </div>
                     </span>
