0440b4ffc5035fefb5e43b431acb65bfb826efd8
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-11-26 11:55:39 +0200

    63. Додав axios.create()

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,16 @@ import *as axios from "axios";
 
 const baseUrl = "https://social-network.samuraijs.com/api/1.0/";
 
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "86e5f8fb-0fbf-4804-b46b-33ed56eeeec0"
+    }
+})
+
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`,
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`,
         { withCredentials: true }).then(response => {
             return response.data;
         });
