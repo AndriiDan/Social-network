c3d09c364e16c2df09921bda0a963013cd3bec30
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-02 12:15:05 +0200

    69. Створив HOC withAuthRedirect для перевірки авторизації.

---
+++ src/hoc/withAuthRedirect.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+export const withAuthRedirect = (Component) => {
+    class RedirectComponent extends React.Component {
+        render() {
+            // перенапрівлення для авторизації при неавторизованому вході
+            if (!props.isAuth) return <Redirect to="/login" />
+            // повертає конкретну компоненту з прокинутими пропсами
+            return <Component {...this.props} />
+        }
+    }
+    return RedirectComponent
+}
