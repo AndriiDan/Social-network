d27c2ca71da18161e02fa784952298cddd489fd3
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-03 12:11:46 +0200

    71. onDoubleClick - зміна status на input з використанням setState().

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 
 class ProfileStatus extends React.Component {
+    // значення за замовчуванням
     state = {
         editMode: false
     }
 
+    // зміна значення editMode при onDoubleClick
     activateEditMode = () => {
-        this.state.editMode = true;
-        this.forceUpdate();
+        this.setState({
+            editMode: true
+        })
     }
 
     render() {
         return (
             <div>
+                {/* якщо editMode: false - відобразить Status */}
                 {!this.state.editMode &&
                     <div>
                         <span className={s.status} onDoubleClick={this.activateEditMode}>Status: {this.props.status}</span>
                     </div>
                 }
+                {/* якщо editMode: true - відобразить input */}
                 {this.state.editMode &&
                     <div>
                         <input value={this.props.status} />
