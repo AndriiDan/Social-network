9e18c3296befa0b342458ee8f0d846d76c3eeef4
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-03 11:15:46 +0200

    71. ProfileStatus - виводить status або input.

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 
 class ProfileStatus extends React.Component {
+    state = {
+        editMode: false
+    }
     render() {
         return (
             <div>
-                <div>
-                    <div>{this.props.status}</div>
-                </div>
-                <div>
-                    <input value={this.props.status} />
-                </div>
+                {!this.state.editMode &&
+                    <div>
+                        <div className={s.status}>Status: {this.props.status}</div>
+                    </div>
+                }
+                {this.state.editMode &&
+                    <div>
+                        <input value={this.props.status} />
+                    </div>
+                }
             </div>
         )
     }
