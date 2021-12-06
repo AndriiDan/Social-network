43f2e5d79118769f940f1dc34d6d68490dae379e
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-06 18:18:38 +0200

    75. Form Login.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    return <div>
+        <h1>LOGIN</h1>
+        <div>
+            <input placeholder={"Login"} />
+        </div>
+        <div>
+            <input placeholder={"Password"} />
+        </div>
+        <div>
+            <input type={"checkbox"} /> remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </div>
 }
 
 export default Login;
