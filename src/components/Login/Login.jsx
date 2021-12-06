9756ec670483678c0494428cb1ed6c4e14a2ab89
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-06 18:20:27 +0200

    75. Form Login form.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 const Login = (props) => {
     return <div>
         <h1>LOGIN</h1>
-        <div>
-            <input placeholder={"Login"} />
-        </div>
-        <div>
-            <input placeholder={"Password"} />
-        </div>
-        <div>
-            <input type={"checkbox"} /> remember me
-        </div>
-        <div>
-            <button>Login</button>
-        </div>
+        <form>
+            <div>
+                <input placeholder={"Login"} />
+            </div>
+            <div>
+                <input placeholder={"Password"} />
+            </div>
+            <div>
+                <input type={"checkbox"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     </div>
 }
 
