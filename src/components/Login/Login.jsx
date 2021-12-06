cd38aa763b9d72ab05fb3004ec87851e56b40ece
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-06 18:23:18 +0200

    75. Виніс LoginForm в окрему компоненту.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
+const LoginForm = (props) => {
+    return <form>
+        <div>
+            <input placeholder={"Login"} />
+        </div>
+        <div>
+            <input placeholder={"Password"} />
+        </div>
+        <div>
+            <input type={"checkbox"} /> remember me
+            </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+}
+
 const Login = (props) => {
     return <div>
         <h1>LOGIN</h1>
-        <form>
-            <div>
-                <input placeholder={"Login"} />
-            </div>
-            <div>
-                <input placeholder={"Password"} />
-            </div>
-            <div>
-                <input type={"checkbox"} /> remember me
-            </div>
-            <div>
-                <button>Login</button>
-            </div>
-        </form>
+        <LoginForm />
     </div>
 }
 
