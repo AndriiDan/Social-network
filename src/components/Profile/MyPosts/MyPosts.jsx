faf2a9a9bb8e32b6641d043ef65cb92120ce6bc2
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-08 16:29:49 +0200

    77. css для FormsControls.

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { maxLengthCreator, required } from '../../../utils/validators/validators';
-import { Textarea } from '../../common/FormsControl/FormsControl';
-import s from './MyPosts.module.css'
+import { Textarea } from '../../common/FormsControls/FormsControls';
+import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
--- src/components/common/FormsControls/FormsControls.jsx
+++ src/components/common/FormsControls/FormsControls.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './FormsControls.module.css';
 
 // ({ input, meta, ...props }) деструкторизація props. В ...props відсутні input, meta.
 export const Textarea = ({ input, meta, ...props }) => {
     return (
-        <div>
+        <div className={styles.formControl + " " + styles.error}>
             <textarea {...input} {...props} />
         </div>
     )
