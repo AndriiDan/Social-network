dc621340979300003983ffd839c6f1f9ab78bd86
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-09 20:44:22 +0200

    77. Зарефактирив форми для Textarea та Input в FormsControls.jsx.

--- src/components/common/FormsControls/FormsControls.jsx
+++ src/components/common/FormsControls/FormsControls.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import styles from './FormsControls.module.css';
 
 // ({ input, meta, ...props }) деструкторизація props. В ...props відсутні input, meta.
-export const Textarea = ({ input, meta, ...props }) => {
+export const FormControl = ({ input, meta, ...props }) => {
     // умова помилки: якщо була взаємодія з елементом і є помилка (умава валідації)
     const hasError = meta.touched && meta.error;
     return (
         // додає стилі. (hasError ? styles.error : "") - якщо виконується умова помилки, додати styles.error, якщо ні - додати пустий рядок
         <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
             <div>
-                <textarea {...input} {...props} />
+                {/* підставляє Textarea, Input */}
+                {props.children}
             </div>
             {/* якщо виконується умова помилки, показати span. Текст помилки міститься в  meta.error */}
             { hasError && <span>{meta.error}</span>}
@@ -17,18 +18,18 @@ export const Textarea = ({ input, meta, ...props }) => {
     )
 }
 
-// ({ input, meta, ...props }) деструкторизація props. В ...props відсутні input, meta.
-export const Input = ({ input, meta, ...props }) => {
-    // умова помилки: якщо була взаємодія з елементом і є помилка (умава валідації)
-    const hasError = meta.touched && meta.error;
+// ...restProps залишкові пропси (без input, meta)
+export const Textarea = (props) => {
+    const { input, meta, ...restProps } = props;
     return (
-        // додає стилі. (hasError ? styles.error : "") - якщо виконується умова помилки, додати styles.error, якщо ні - додати пустий рядок
-        <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            {/* якщо виконується умова помилки, показати span. Текст помилки міститься в  meta.error */}
-            { hasError && <span>{meta.error}</span>}
-        </div>
+        <FormControl {...props}><textarea {...input} {...restProps} /></FormControl>
+    )
+}
+
+// ...restProps залишкові пропси (без input, meta)
+export const Input = (props) => {
+    const { input, meta, ...restProps } = props;
+    return (
+        <FormControl {...props}><input {...input} {...restProps} /></FormControl>
     )
 }
