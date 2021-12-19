f55a3f6311ecb2b06270b8da29670a8dea6a70b3
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-19 23:44:51 +0200

    83. reselect для users.

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,7 +1,15 @@
-export const getUsers = (state) => {
-    // filter - пропустити всіх юзерів
-    return state.usersPage.users.filter(u => true);
+import { createSelector } from "reselect";
+
+// отримати users з state. Урок 82
+const getUsersSelector = (state) => {
+    return state.usersPage.users;
 }
+
+// урок 83. reselect - дія mapStateToProps без render сторінки; див. console на сторінці users
+export const getUsers = createSelector(getUsersSelector, (users) => {
+    // filter - пропустити всіх юзерів
+    return users.filter(u => true);
+})
 
 export const getPageSize = (state) => {
     return state.usersPage.pageSize;
