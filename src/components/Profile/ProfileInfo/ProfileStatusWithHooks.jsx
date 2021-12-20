307dc55e937b94ba17baa42fc7a94140c10f7cd5
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-20 22:11:41 +0200

    84. ProfileStatusWithHooks 2. useState().

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './ProfileInfo.module.css';
 
 const ProfileStatusWithHooks = (props) => {
+
+    // створимо локальний стейт. useState повертає масив з двома елементами [0, 1]
+    let stateWithSetState = useState(false);
+    // перший елемент масиву [0] - значення editMode. Значення зберігається в React
+    let editMode = stateWithSetState[0];
+    // другий елемент масиву [1] - функція, яка змінює одиночне значення editMode
+    let setEditMode = stateWithSetState[1];
+
     return (
         <div>
-            {
+            {/* якщо editMode = false, то показати span */}
+            { !editMode &&
                 <div>
                     <span>Status: {props.status || "-----"}</span>
                 </div>
             }
-            { false &&
+            {/* якщо editMode = true, то показати input */}
+            { editMode &&
                 <input autoFocus={true} />
             }
         </div>
