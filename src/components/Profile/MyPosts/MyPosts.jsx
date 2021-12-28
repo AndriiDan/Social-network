fcbf4524df89100c4309d2b6f1cde1c702f3f0ab
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2021-12-28 11:38:47 +0200

    87. React.memo для MyPosts.

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,41 +1,32 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { maxLengthCreator, required } from '../../../utils/validators/validators';
 import { Textarea } from '../../common/FormsControls/FormsControls';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-// всередині PureComponent вже вмонтований shouldComponentUpdate (для порівняння props і state - чи перемальовувати компоненту) 
-class MyPosts extends PureComponent {
+// всередині React.memo вже вмонтовано порівняння props і state - чи перемальовувати компоненту
+const MyPosts = React.memo((props) => {
 
-  // через 3 сек змінити стейт
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ a: 12 })
-    }, 3000)
+  console.log("RENDER MyPosts");
+  // Створює новий масив з постів
+  let postsElement = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
+
+  // Функція, яка додає пост
+  let onAddPost = (values) => {
+    props.addPost(values.newPostText);
   }
 
-  render() {
-    console.log("RENDER MyPosts");
-    // Створює новий масив з постів
-    let postsElement = this.props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
-
-    // Функція, яка додає пост
-    let onAddPost = (values) => {
-      this.props.addPost(values.newPostText);
-    }
-
-    return <div className={s.content}>
-      <div className={s.postsBlock}>
-        <h3>My posts</h3>
-        <AddNewPostFormRedux onSubmit={onAddPost} />
-        <div className={s.posts}>
-          {postsElement}
-        </div>
+  return <div className={s.content}>
+    <div className={s.postsBlock}>
+      <h3>My posts</h3>
+      <AddNewPostFormRedux onSubmit={onAddPost} />
+      <div className={s.posts}>
+        {postsElement}
       </div>
     </div>
-  }
-}
+  </div>
+});
 
 // константа для максимальної довжини в 10 символів 
 const maxLength10 = maxLengthCreator(10);
