7f743398894fc9b7e5c56e1d3a1a2b40211a3b89
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-01-04 22:02:42 +0200

    89. profile-reducer.test.js - два теста.

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,20 +1,35 @@
 import profileReducer, { addPostActionCreator } from "./profile-reducer";
 import React from 'react';
 
+let state = {
+    posts: [
+        { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+        { id: 2, message: 'It\'s my first post.', likesCount: 20 }
+    ]
+};
+
+
 it('length posts should be increment', () => {
 
     // 1. test data; вхідні дані
     let action = addPostActionCreator("it-kamasutra.com");
-    let state = {
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likesCount: 15 },
-            { id: 2, message: 'It\'s my first post.', likesCount: 20 }
-        ]
-    };
 
     // 2. action; провести тест
     let newState = profileReducer(state, action);
 
     // 3. expectation; очікуємо, що length стане 3
     expect(newState.posts.length).toBe(3);
+});
+
+
+it('message of new posts should be correct', () => {
+
+    // 1. test data; вхідні дані
+    let action = addPostActionCreator("it-kamasutra.com");
+
+    // 2. action; провести тест
+    let newState = profileReducer(state, action);
+
+    // 3. expectation; очікуємо, що length стане 3
+    expect(newState.posts[2].message).toBe("it-kamasutra.com");
 });
