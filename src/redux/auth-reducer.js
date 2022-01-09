25630ed5c4f30dad44eec1ce4da4dea601075dfc
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-01-09 20:02:04 +0200

    90.Рефакторінг async await для auth-reducer.js.

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -26,49 +26,39 @@ export const setAuthUserData = (userId, email, login, isAuth) =>
     ({ type: SET_USER_DATA, payload: { userId, email, login, isAuth } })
 
 // thunk для авторизації
-export const getAuthUserData = () => (dispatch) => {
-    // return для повернення промісу для initializedSuccess в app-reducer.js
-    return authAPI.me()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                let { id, email, login } = response.data.data;
-                dispatch(setAuthUserData(id, email, login, true));
-            }
-        });
+export const getAuthUserData = () => async (dispatch) => {
+    // async - await - асинхронно (замість .then) для повернення промісу для initializedSuccess в app-reducer.js
+    let response = await authAPI.me();
+    if (response.data.resultCode === 0) {
+        let { id, email, login } = response.data.data;
+        dispatch(setAuthUserData(id, email, login, true));
+    }
 }
 
 // thunk для авторизації login
-export const login = (email, password, rememberMe) => (dispatch) => {
-
-    // зупинка форми (LoginReduxform = reduxForm({ form: 'login' })(LoginForm)); помилка по LoginForm - <Field name={"email"}
-    // let action = stopSubmit("login", { _error: "Common error" });
-    // dispatch(action);
-    // return;
-
-    authAPI.login(email, password, rememberMe)
-        .then(response => {
-            // якщо resultCode === 0, то авторизуватися
-            if (response.data.resultCode === 0) {
-                // dispatch - внесення даних авторизації (з formData) 
-                dispatch(getAuthUserData());
-            } else {
-                // інакше, зупинка форми (LoginReduxform = reduxForm({ form: 'login' })(LoginForm)); помилка по LoginForm - <Field name={"email"}
-                // якщо messages.length > 0 відобразити response.data.messages, інакше - повідомлення "Some error"
-                let message = response.data.messages.length > 0 ? response.data.messages : "Some error";
-                dispatch(stopSubmit("login", { _error: message }))
-            }
-        });
+export const login = (email, password, rememberMe) => async (dispatch) => {
+    // async - await - асинхронно (замість .then)
+    let response = await authAPI.login(email, password, rememberMe);
+    // якщо resultCode === 0, то авторизуватися
+    if (response.data.resultCode === 0) {
+        // dispatch - внесення даних авторизації (з formData) 
+        dispatch(getAuthUserData());
+    } else {
+        // інакше, зупинка форми (LoginReduxform = reduxForm({ form: 'login' })(LoginForm)); помилка по LoginForm - <Field name={"email"}
+        // якщо messages.length > 0 відобразити response.data.messages, інакше - повідомлення "Some error"
+        let message = response.data.messages.length > 0 ? response.data.messages : "Some error";
+        dispatch(stopSubmit("login", { _error: message }))
+    }
 }
 
 // thunk для logout
-export const logout = () => (dispatch) => {
-    authAPI.logout()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                // dispatch - видалення (обнулення) даних авторизації 
-                dispatch(setAuthUserData(null, null, null, false));
-            }
-        });
+export const logout = () => async (dispatch) => {
+    // async - await - асинхронно (замість .then)
+    let response = await authAPI.logout();
+    if (response.data.resultCode === 0) {
+        // dispatch - видалення (обнулення) даних авторизації 
+        dispatch(setAuthUserData(null, null, null, false));
+    }
 }
 
 export default authReducer;
