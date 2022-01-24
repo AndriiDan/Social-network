b6d73826cda015e8ca16bac21c9c23203762a276
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-01-24 15:07:49 +0200

    93. Порції юзерів для Paginator.

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Paginator.module.css";
 
 // Відображення номерів сторінок
-let Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged }) => {
+let Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10 }) => {
 
     // інформація, про загальну к-сть сторінок
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
@@ -14,19 +14,38 @@ let Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged }) => {
         pages.push(i);
     }
 
+    // к-сть порцій сторінок юзерів
+    const portionCount = Math.ceil(pagesCount / portionSize);
+    // локальний стейт - зміна номера порції
+    const [portionNumber, setPortionNumber] = useState(1);
+    // ліва межа порції
+    const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    // права межа порції
+    const rightPortionPageNumber = portionNumber * portionSize;
+
     return <div>
-        <div>
-            {/* сформовуємо рядок з номерацією сторінок */}
-            {pages.map(p => {
+        {/* Кнопка - попередня порція юзерів */}
+        {portionNumber > 1 &&
+            <button onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button>
+        }
+
+        {/* сформовуємо рядок з номерацією сторінок */}
+        {pages
+            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map((p) => {
                 return <span
                     // виділяє активну сторінку
                     className={currentPage === p && styles.selectedPage}
+                    key={p}
                     // перемикає сторінку з юзерами
-                    onClick={() => { onPageChanged(p) }}>
+                    onClick={(e) => { onPageChanged(p) }}>
                     {p}
                 </span>
             })}
-        </div>
+
+        {/* Кнопка - наступна порція юзерів */}
+        {portionCount > portionNumber &&
+            <button onClick={() => { setPortionNumber(portionNumber + 1) }}>NEXT</button>}
     </div>
 
 }
